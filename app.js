7556bf1d3244f08d8c07e5ada6628dbaaa0a997b
brunoperetti <brunoperetti10@gmail.com>
2021-05-10 21:12:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var el = document.getElementById('loginBtn');
+if(el){
+  el.addEventListener('click', crearEscuela, false);
+}
+
+//document.getElementById("log1").addEventListener("click", crearEscuela);
+
+
+function crearEscuela (){
+    fetch('http://localhost:8080/api/escuela', {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json"
+    }, 
+    body: JSON.stringify({
+        'codigoUnicoEstablecimiento': document.getElementById("codigo").value,
+        'clave': document.getElementById("psw").value
+    })
+    
+})
+.then(response => response.json())
+.then(data => console.log(data));
+
+}
